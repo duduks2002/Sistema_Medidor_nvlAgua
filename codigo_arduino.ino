d1b83b1396b8031fcaa7ea256ba54cd3734f2ebc
Edward Abromovik <82452022+duduks2002@users.noreply.github.com>
2024-05-17 07:14:53 -0300

    Update codigo_arduino.ino

--- codigo_arduino.ino
+++ codigo_arduino.ino
@@ -1,36 +1,47 @@
 
+int pino_led_g = 7;
+int pino_led = 5;
 //Pino ligado ao sensor de nivel de liquido
 int pinosensor = 3;
-//Led verde
-int pino_led_cheio = 6;
-//Led vermelho
-int pino_led_vazio = 7;
 
 void setup()
 {
   Serial.begin(9600);
   pinMode(pinosensor, INPUT);
-  pinMode(pino_led_cheio, OUTPUT);
-  pinMode(pino_led_vazio, OUTPUT);
+  pinMode(pino_led, OUTPUT);
+  pinMode(pino_led_g, OUTPUT);
+  
 }
 
 void loop()
 {
-  int estado = digitalRead(pinosensor);
-  Serial.print("Estado sensor : ");
-  Serial.println(estado);
-  switch(estado)
+  //int estado = digitalRead(pinosensor);
+  /*Serial.print("Estado sensor : ");*/
+  //Serial.println(estado);
+  char command = Serial.read();
+  
+ /* switch(estado)
   {
-  case 0:
-    digitalWrite(pino_led_vazio, LOW);
-    digitalWrite(pino_led_cheio, HIGH);
-    Serial.println("pino levantado");
-    break;
-  case 1:
-    digitalWrite(pino_led_vazio, HIGH);
-    digitalWrite(pino_led_cheio, LOW);
-    Serial.println("pino abaixado");
-    break;
-  } 
-  delay(1000);
+    case 0:
+      Serial.println("Abaixo do Limite.");
+      digitalWrite(pino_led,LOW);
+      break;
+    case 1:
+      Serial.println("LIMITE ALCANÇADO !!");
+      digitalWrite(pino_led, HIGH);
+      break;
+  }*/
+  
+  if (Serial.available() > 1) {
+    char command = Serial.read();
+  
+    if (command == '7') {
+      digitalWrite(pino_led_g, HIGH);
+    }
+    else {
+      digitalWrite(pino_led_g, LOW);
+    }
+    delay(500)
+  }
+  delay(5000);
 }
